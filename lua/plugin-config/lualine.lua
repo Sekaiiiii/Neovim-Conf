3da1201125ac78b5590180b81de5c747ab1570f8
sekaiiiii <qq738359456@gmail.com>
2023-08-01 22:36:15 +0800

    feat: null-ls简单调整

--- lua/plugin-config/lualine.lua
+++ lua/plugin-config/lualine.lua
@@ -8,32 +8,32 @@ lualine.setup({
   options = {
     icons_enabled = true,
     theme = "auto",
-    component_separators = { left = ">>", right = "<<" },
+    component_separators = { left = ">", right = "<" },
     section_separators = { left = "", right = "" },
-    disabled_filetypes = { statusline = {}, winbar = {} },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = { statusline = 1000, tabline = 1000, winbar = 1000 },
+    globalstatus = true,
+    refresh = {
+      statusline = 100,
+      tabline = 1000,
+      winbar = 1000,
+    },
   },
   sections = {
-    lualine_a = { "mode" },
-    lualine_b = { "branch", "diff", "diagnostics" },
+    lualine_a = {
+      {
+        "mode",
+        fmt = function(str)
+          return str:sub(1, 1) .. string.lower(str:sub(2, #str))
+        end,
+      },
+    },
+    lualine_b = { "branch", "diff" },
     lualine_c = {
-      "filename",
       require("auto-session.lib").current_session_name,
+      "filename",
     },
     lualine_x = { "encoding", "fileformat", "filetype" },
     lualine_y = { "progress" },
     lualine_z = { "location" },
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = { "filename" },
-    lualine_x = { "location" },
-    lualine_y = {},
-    lualine_z = {},
   },
   tabline = {},
   winbar = {},
