1123f77a2a75f57f7a171b6c047080ee6ba7d059
sekaiiiii <qq738359456@gmail.com>
2024-01-09 22:50:12 +0800

    feat: 包管理工具由pakcer升级到lazy

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,176 +1,194 @@
-require("packer").startup({
-	function(use)
-		-- packer 插件管理
-		use("wbthomason/packer.nvim")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-		-- nvim-tree 文件管理
-		use({
-			"kyazdani42/nvim-tree.lua",
-			requires = "kyazdani42/nvim-web-devicons",
-		})
-		require("plugin-config.nvim-tree")
+local plugins = {
+  {
+    "kyazdani42/nvim-tree.lua",
+    dependencies = { "kyazdani42/nvim-web-devicons" },
+    config = function()
+      require("plugin-config.nvim-tree")
+    end,
+  },
+  {
+    "rmagatti/auto-session",
+    config = function()
+      require("plugin-config.auto-session")
+    end,
+  },
+  {
+    "glepnir/dashboard-nvim",
+    config = function()
+      require("plugin-config.dashboard")
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "kyazdani42/nvim-web-devicons" },
+    config = function()
+      require("plugin-config.lualine")
+    end,
+  },
+  {
+    "akinsho/bufferline.nvim",
+    dependencies = { "kyazdani42/nvim-web-devicons" },
+    config = function()
+      require("plugin-config.bufferline")
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("plugin-config.telescope")
+    end,
+  },
+  {
+    "ggandor/leap.nvim",
+    config = function()
+      require("plugin-config.leap")
+    end,
+  },
+  {
+    "ggandor/flit.nvim",
+    config = function()
+      require("plugin-config.flit")
+    end,
+  },
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      require("plugin-config.comment")
+    end,
+  },
+  {
+    "akinsho/toggleterm.nvim",
+    config = function()
+      require("plugin-config.toggleterm")
+    end,
+  },
+  {
+    "kylechui/nvim-surround",
+    config = function()
+      require("plugin-config.nvim-surround")
+    end,
+  },
+  {
+    "windwp/nvim-autopairs",
+    config = function()
+      require("plugin-config.nvim-autopairs")
+    end,
+  },
+  { "famiu/bufdelete.nvim" }, -- theme
+  { "savq/melange-nvim" },
+  { "folke/tokyonight.nvim" },
+  { "projekt0n/github-nvim-theme" }, -- treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
 
-		-- auto-session 会话管理
-		use({ "rmagatti/auto-session" })
-		require("plugin-config.auto-session")
+      "p00f/nvim-ts-rainbow",
+      "JoosepAlviste/nvim-ts-context-commentstring",
+      "windwp/nvim-ts-autotag",
+      "nvim-treesitter/nvim-treesitter-refactor",
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    config = function()
+      require("plugin-config.nvim-treesitter")
+    end,
+  },
+  {
+    "romgrk/nvim-treesitter-context",
+    config = function()
+      require("plugin-config.treesitter-context")
+    end,
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("plugin-config.gitsigns")
+    end,
+  },
+  {
+    "akinsho/git-conflict.nvim",
+    config = function()
+      require("plugin-config.git-conflict")
+    end,
+  },
+  {
+    "sindrets/diffview.nvim",
+    config = function()
+      require("plugin-config.diffview")
+    end,
+  },
+  { "williamboman/mason.nvim" },
+  { "williamboman/mason-lspconfig" },
+  { "neovim/nvim-lspconfig" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/cmp-cmdline" },
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-vsnip" },
+  { "hrsh7th/vim-vsnip" },
+  { "saadparwaiz1/cmp_luasnip" },
+  { "rafamadriz/friendly-snippets" },
+  { "onsails/lspkind-nvim" },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    config = function()
+      require("plugin-config.null-ls")
+    end,
+  },
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async" },
+    config = function()
+      require("plugin-config.nvim-ufo")
+    end,
+  },
+  {
+    "simrat39/symbols-outline.nvim",
+    config = function()
+      require("plugin-config.symbols-outline")
+    end,
+  },
+  {
+    "folke/edgy.nvim",
+    init = function()
+      vim.opt.laststatus = 3
+      vim.opt.splitkeep = "screen"
+    end,
+    config = function()
+      require("plugin-config.edgy")
+    end,
+  },
+  {
+    "rcarriga/nvim-notify",
+    config = function()
+      require("plugin-config.nvim-notify")
+    end,
+  },
+  {
+    "folke/noice.nvim",
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim", -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+  },
+}
+local opts = {}
 
-		-- lualine 底部状态栏
-		use({
-			"nvim-lualine/lualine.nvim",
-			requires = { "nvim-tree/nvim-web-devicons", opt = true },
-		})
-		require("plugin-config.lualine")
-
-		-- dashboard
-		use({ "glepnir/dashboard-nvim" })
-		require("plugin-config.dashboard")
-
-		-- buffer line
-		use({
-			"akinsho/bufferline.nvim",
-			tag = "*",
-			requires = "nvim-tree/nvim-web-devicons",
-		})
-		require("plugin-config.bufferline")
-
-		-- telescope
-		use({
-			"nvim-telescope/telescope.nvim",
-			tag = "0.1.4",
-			requires = { { "nvim-lua/plenary.nvim" } },
-		})
-		require("plugin-config.telescope")
-
-		-- leap
-		use({
-			"ggandor/leap.nvim",
-		})
-		require("plugin-config.leap")
-
-		-- flit
-		use({
-			"ggandor/flit.nvim",
-		})
-		require("plugin-config.flit")
-
-		-- comment
-		use({ "numToStr/Comment.nvim" })
-		require("plugin-config.comment")
-
-		-- nvim-toggleterm
-		use({ "akinsho/toggleterm.nvim" })
-		require("plugin-config.toggleterm")
-
-		-- nvim-surround
-		use({ "kylechui/nvim-surround" })
-		require("plugin-config.nvim-surround")
-
-		-- nvim-autopairs
-		use({ "windwp/nvim-autopairs" })
-		require("plugin-config.nvim-autopairs")
-
-		-- buf delete
-		use("famiu/bufdelete.nvim")
-
-		-- color themes
-		use("savq/melange-nvim")
-		use("folke/tokyonight.nvim")
-		use("projekt0n/github-nvim-theme")
-
-		-- treesitter
-		use({
-			"nvim-treesitter/nvim-treesitter",
-			requires = {
-				{ "p00f/nvim-ts-rainbow" },
-				{ "JoosepAlviste/nvim-ts-context-commentstring" },
-				{ "windwp/nvim-ts-autotag" },
-				{ "nvim-treesitter/nvim-treesitter-refactor" },
-				{ "nvim-treesitter/nvim-treesitter-textobjects" },
-			},
-		})
-		require("plugin-config.nvim-treesitter")
-
-		use({
-			"romgrk/nvim-treesitter-context",
-		})
-		require("plugin-config.treesitter-context")
-
-		-- gitsigns
-		use({ "lewis6991/gitsigns.nvim" })
-		require("plugin-config.gitsigns")
-
-		-- git-conflict
-		use({ "akinsho/git-conflict.nvim", version = "*" })
-		require("plugin-config.git-conflict")
-
-		-- diffview
-		use({ "sindrets/diffview.nvim" })
-		require("plugin-config.diffview")
-
-		-- lsp corrlation
-		use({ "williamboman/mason.nvim" })
-		use({ "williamboman/mason-lspconfig" })
-		use({ "neovim/nvim-lspconfig" })
-
-		-- nvim-cmp
-		use({ "hrsh7th/cmp-nvim-lsp" }) -- { name = nvim_lsp }
-		use({ "hrsh7th/cmp-buffer" }) -- { name = 'buffer' },
-		use({ "hrsh7th/cmp-path" }) -- { name = 'path' }
-		use({ "hrsh7th/cmp-cmdline" }) -- { name = 'cmdline' }
-		use({ "hrsh7th/nvim-cmp" })
-
-		-- vsnip
-		use("hrsh7th/cmp-vsnip") -- { name = 'vsnip' }
-		use("hrsh7th/vim-vsnip")
-		use("L3MON4D3/LuaSnip")
-		use("saadparwaiz1/cmp_luasnip")
-		use("rafamadriz/friendly-snippets")
-
-		-- lspkind
-		use("onsails/lspkind-nvim")
-
-		-- null-ls
-		use("jose-elias-alvarez/null-ls.nvim")
-		require("plugin-config.null-ls")
-
-		-- nvim-ufo
-		use({ "kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async" })
-		require("plugin-config.nvim-ufo")
-
-		--symbol-outline
-		use("simrat39/symbols-outline.nvim")
-		require("plugin-config.symbols-outline")
-
-		--edgy
-		use({
-			"folke/edgy.nvim",
-			init = function()
-				vim.opt.laststatus = 3
-				vim.opt.splitkeep = "screen"
-			end,
-		})
-		require("plugin-config.edgy")
-
-		-- nvim-notify
-		use("rcarriga/nvim-notify")
-		require("plugin-config.nvim-notify")
-
-		-- noice
-		use({
-			"folke/noice.nvim",
-			dependencies = {
-				-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-				"MunifTanjim/nui.nvim",
-				-- OPTIONAL:
-				--   `nvim-notify` is only needed, if you want to use the notification view.
-				--   If not available, we use `mini` as the fallback
-				"rcarriga/nvim-notify",
-			},
-		})
-		require("plugin-config.noice")
-	end,
-}, {
-	git = {
-		default_url_format = "git@github.com:%s",
-	},
-})
+require("lazy").setup(plugins, opt)
